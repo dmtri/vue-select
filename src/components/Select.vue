f6bd29ecec24ce11c9533e0254c4f33593c970a2
Duc <dmtrinh15@gmail.com>
2017-04-17 17:44:08 +0700

    change loader position

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -281,13 +281,13 @@
 
       <i v-if="!noDrop" ref="openIndicator" role="presentation" class="open-indicator"></i>
 
-      <slot name="spinner">
-        <div class="spinner" v-show="mutableLoading">Loading...</div>
-      </slot>
     </div>
 
     <transition :name="transition">
       <ul ref="dropdownMenu" v-if="dropdownOpen && search.length" class="dropdown-menu" :style="{ 'max-height': maxHeight }">
+        <slot name="spinner">
+          <div class="spinner" v-show="mutableLoading">Loading...</div>
+        </slot>
         <li v-for="(option, index) in filteredOptions" v-bind:key="index" :class="{ active: isOptionSelected(option), highlight: index === typeAheadPointer }" @mouseover="typeAheadPointer = index">
           <a @mousedown.prevent="select(option)">
             {{ getOptionLabel(option) }}
